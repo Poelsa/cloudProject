46faa58439dcdf41579f743bac1b71fd037f841b
Poelsa <joel.begnert@gmail.com>
2016-04-17 09:34:49 +0000

    Added dotstar API and a test script

--- LEDtest.py
+++ LEDtest.py
@@ -1,10 +1,48 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python2
+
+import argparse
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+from dotstar.dotstar import Adafruit_DotStar
+
+parser = argparse.ArgumentParser()
+parser.add_argument("led", help="index of led", type=int)
+offGroup = parser.add_mutually_exclusive_group()
+offGroup.add_argument("--off", help="turns off LED", action="store_true")
+offGroup.add_argument("--alloff", help="turns off all LEDs", action="store_true")
+parser.add_argument("-r", "--red", type=int, help="amount of red [0:255]")
+parser.add_argument("-g", "--green", type=int, help="amount of green [0:255]")
+parser.add_argument("-b", "--blue", type=int, help="amount of blue [0:255]")
+
+args = parser.parse_args()
+
+numpixels = 30
+datapin   = 20
+clockpin  = 21
+strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
+
+
+strip.begin()
+strip.setBrightness(64)
+
+if args.alloff:
+    for x in range(0, numpixels-1):
+        strip.setPixelColor(x, 0)
+    strip.show()
+else:
+    led = args.led
+    color = 0x00
+    if args.red:
+        color = color | (args.red << 8)
+    if args.green:
+        color = color | (args.green << 16)
+    if args.blue:
+        color = color | args.blue
+
+    if args.off:
+        color = 0
+
+    strip.setPixelColor(args.led, color)
+
+    while True:
+        strip.show()
+        time.sleep(1.0/25)
