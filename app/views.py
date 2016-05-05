6480126f5e5d09af1d270463b4f91f03931c2b9e
Poelsa <joel.begnert@gmail.com>
2016-05-05 13:16:32 +0000

    Enabling LEDs from web page, ugly but possible

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, jsonify, request
 from app import app
 from .forms import LoginForm
+import subprocess
 
 @app.route('/')
 @app.route('/index')
@@ -15,6 +16,11 @@ def led():
 @app.route('/led_config', methods=['POST'])
 def light_led():
     #Get values from form, and do the dotstar script
+    i = request.form['index']
+    r = request.form['red']
+    g = request.form['green']
+    b = request.form['blue']
+#    subprocess.call("sudo python2 /home/pi/Documents/flaskDir/app/led/LEDtest.py " + i + " -r " + r + " -g " + g + " -b " + b, shell=True)
     return jsonify({
         'index': request.form['index']
         })
